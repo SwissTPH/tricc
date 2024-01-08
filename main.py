6e3f60bc6564b83311d05fe9671b2c4c7f88f4d8
Rukshan <arkruka@gmail.com>
2024-01-08 17:04:50 +0100

    feat: :sparkles: read parameters from a configuration file

--- main.py
+++ main.py
@@ -4,40 +4,45 @@ import logging
 import os
 import sys
 import gc
+import tomllib
+from pathlib import Path
+import yaml
 
 # set up logging to file
 from tricc.models.lang import SingletonLangClass
 
-#gettext.bindtextdomain('tricc', './locale/')
-#gettext.textdomain('tricc')
+# gettext.bindtextdomain('tricc', './locale/')
+# gettext.textdomain('tricc')
 langs = SingletonLangClass()
 
-#fr =  gettext.translation('tricc', './locales' , languages=['fr'])
-#fr.install()
-#en =  gettext.translation('tricc', './locales' , languages=['en'])
-#en.install()
+# fr =  gettext.translation('tricc', './locales' , languages=['fr'])
+# fr.install()
+# en =  gettext.translation('tricc', './locales' , languages=['en'])
+# en.install()
 
 
-#langs.add_trad('fr', fr)
-#langs.add_trad('en', en)
+# langs.add_trad('fr', fr)
+# langs.add_trad('en', en)
 
 from tricc.strategies.input.drawio import DrawioStrategy
 from tricc.strategies.input.medalcreator import MedalCStrategy
-#from tricc.serializers.medalcreator import execute
+
+# from tricc.serializers.medalcreator import execute
 
 from tricc.strategies.output.xls_form import XLSFormStrategy
 from tricc.strategies.output.xlsform_cdss import XLSFormCDSSStrategy
 from tricc.strategies.output.xlsform_cht import XLSFormCHTStrategy
 
 
-def setup_logger(logger_name,
-                 log_file, 
-                 level=logging.INFO, 
-                 formatting  ='[%(asctime)s] {%(pathname)s:%(lineno)d} %(levelname)s - %(message)s'):
-     
+def setup_logger(
+    logger_name,
+    log_file,
+    level=logging.INFO,
+    formatting="[%(asctime)s] {%(pathname)s:%(lineno)d} %(levelname)s - %(message)s",
+):
     l = logging.getLogger(logger_name)
     formatter = logging.Formatter(formatting)
-    file_handler = logging.FileHandler(log_file, mode='w')
+    file_handler = logging.FileHandler(log_file, mode="w")
     file_handler.setFormatter(formatter)
     stream_handler = logging.StreamHandler()
     stream_handler.setFormatter(formatter)
@@ -46,99 +51,133 @@ def setup_logger(logger_name,
     l.addHandler(file_handler)
 
 
-
-logger = logging.getLogger('default')
+logger = logging.getLogger("default")
 
 # set up logging to console
 console = logging.StreamHandler()
 console.setLevel(logging.INFO)
 # set a format which is simpler for console use
-formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
+formatter = logging.Formatter("%(name)-12s: %(levelname)-8s %(message)s")
 console.setFormatter(formatter)
 # add the handler to the root logger
-logging.getLogger('').addHandler(console)
+logging.getLogger("").addHandler(console)
 
 LEVELS = {
-    'd':logging.DEBUG,
-    'w':logging.WARNING,
-    'i':logging.INFO,
-
+    "d": logging.DEBUG,
+    "w": logging.WARNING,
+    "i": logging.INFO,
 }
 
 
 def print_help():
-    print('-i / --input draw.io filepath (MANDATORY) or directory containing drawio files')
-    print('-o / --output xls file ')
-    print('-d form_id ')
-    print('-s L4 system/strategy (odk, cht, cc)')
-    print('-h / --help print that menu')
+    print(
+        "-i / --input draw.io filepath (MANDATORY) or directory containing drawio files"
+    )
+    print("-o / --output xls file ")
+    print("-d form_id ")
+    print("-s L4 system/strategy (odk, cht, cc)")
+    print("-h / --help print that menu")
 
-    
+
 if __name__ == "__main__":
-    gc.disable()
+    # gc.disable()
 
-    system='odk'
-    in_filepath= None
-    out_path=None
-    form_id=None
-    debug_level=None
-    trad = False
-    
-    input_strategy = 'DrawioStrategy'
-    output_strategy= 'XLSFormStrategy'
-    try:
-      opts, args = getopt.getopt(sys.argv[1:],"hti:o:s:I:O:l:",["input=","output=","help","trads"])
-    except getopt.GetoptError:
+    config = sys.argv
+
+    if config[1] is None:
+        logger.error("no config file provided, aborting")
+        exit()
+    elif config[1] == "-h" or config[1] == "--help":
         print_help()
-        sys.exit(2)
-    for opt, arg in opts:
-        if opt in ('-h', '--help'):
-            print_help()
-            sys.exit()
-        elif opt in ("-i", "--input"):
-            in_filepath = arg
-        elif opt == "-o":
-            out_path = arg
-        elif opt == "-I":
-            input_strategy = arg
-        elif opt == "-O":
-            output_strategy = arg
-        elif opt == "-d":
-            form_id = arg
-        elif opt == "-l":
-            debug_level = arg
-        elif opt in ("-t", "--trads"):
-            trad = True
+        exit()
+    else:
+        base_path = Path(__file__).parent.resolve()
+        with open(Path(base_path, "baseconfig.toml"), "rb") as c:
+            base_config = tomllib.load(c)
+            upload_path = base_config["upload_folder"]
+
+        toml_config_path = Path(upload_path + config[1] + ".toml")
+        if Path.is_file(toml_config_path):
+            with open(toml_config_path, "rb") as f:
+                params = tomllib.load(f)
+        else:
+            yaml_config_path = Path(upload_path + config[1] + ".yaml")
+            print(yaml_config_path)
+            with open(yaml_config_path, "rb") as f:
+                params = yaml.load(f, Loader=yaml.SafeLoader)
+
+    system = params["system"]
+    # in_filepath= None
+    in_filepath = params["in_filepath"]
+    # out_path=None
+    out_path = params["out_path"]
+    # form_id=None
+    form_id = params["form_id"]
+    # debug_level=None
+    debug_level = params["debug_level"]
+    # trad = False
+    trad = params["trad"]
+    # input_strategy = 'DrawioStrategy'
+    input_strategy = params["input_strategy"]
+    # output_strategy= 'XLSFormStrategy'
+    output_strategy = params["output_strategy"]
+    # conversion_ID
+    conversion_id = str(params["conversion_ID"])
+
     if in_filepath is None:
         print_help()
-        sys.exit(2)
-    
-    
+        exit()
+
+    # try:
+    #   opts, args = getopt.getopt(sys.argv[1:],"hti:o:s:I:O:l:",["input=","output=","help","trads"])
+    # except getopt.GetoptError:
+    #     print_help()
+    #     sys.exit(2)
+    # for opt, arg in opts:
+    #     if opt in ('-h', '--help'):
+    #         print_help()
+    #         sys.exit()
+    #     elif opt in ("-i", "--input"):
+    #         in_filepath = arg
+    #     elif opt == "-o":
+    #         out_path = arg
+    #     elif opt == "-I":
+    #         input_strategy = arg
+    #     elif opt == "-O":
+    #         output_strategy = arg
+    #     elif opt == "-d":
+    #         form_id = arg
+    #     elif opt == "-l":
+    #         debug_level = arg
+    #     elif opt in ("-t", "--trads"):
+    #         trad = True
+    # if in_filepath is None:
+    #     print_help()
+    #     sys.exit(2)
+
     if debug_level is not None:
-        setup_logger('default', "debug.log", LEVELS[debug_level])
+        setup_logger("default", "debug.log", LEVELS[debug_level])
     elif "pydevd" in sys.modules:
-        setup_logger('default', "debug.log", logging.DEBUG)
+        setup_logger("default", "debug.log", logging.DEBUG)
     else:
-        setup_logger('default', "debug.log", logging.INFO)
-        
+        setup_logger("default", "debug.log", logging.INFO)
+
     pre, ext = os.path.splitext(in_filepath)
     if out_path is None:
         # if output file path not specified, just chagne the extension
-        out_path= os.path.dirname(pre) 
+        out_path = os.path.dirname(pre)
     strategy = globals()[input_strategy](in_filepath)
     logger.info(f"build the graph from strategy {input_strategy}")
-    media_path = os.path.join(out_path, "media-tmp")
-    start_page, pages= strategy.execute(in_filepath,media_path)
-    
+    media_path = os.path.join(out_path, conversion_id, "media-tmp")
+    start_page, pages = strategy.execute(in_filepath, media_path)
+
     strategy = globals()[output_strategy](out_path)
 
     logger.info("Using strategy {}".format(strategy.__class__))
     logger.info("update the node with basic information")
     # create constraints, clean name
-    
-    strategy.execute(start_page, pages=pages)
-    
+
+    strategy.execute(start_page, pages=pages, conversion_id=conversion_id)
 
     if trad:
-        langs.to_po_file('./trad.po')
-     
+        langs.to_po_file("./trad.po")
--- tricc/strategies/output/base_output_strategy.py
+++ tricc/strategies/output/base_output_strategy.py
@@ -3,80 +3,89 @@ import logging
 from tricc.models.tricc import stashed_node_func
 
 
-logger = logging.getLogger('default')
+logger = logging.getLogger("default")
 
 
 class BaseOutPutStrategy:
-    processes = ['main']
+    processes = ["main"]
 
     output_path = None
-    # list of supported processes for the strategy, 
+    # list of supported processes for the strategy,
     # the order of the list will be apply
-    
+
     def __init__(self, output_path):
         self.output_path = output_path
-    
-    
-    def execute(self, start_pages, pages):
-        if 'main' in start_pages:
+
+    def execute(self, start_pages, pages, conversion_id):
+        if "main" in start_pages:
             self.process_base(start_pages, pages=pages)
         else:
             logger.error("Main process required")
 
-        
         logger.info("generate the relevance based on edges")
 
-        
-        
         # create relevance Expression
         self.process_relevance(start_pages, pages=pages)
         logger.info("generate the calculate based on edges")
-        
+
         # create calculate Expression
         self.process_calculate(start_pages, pages=pages)
         logger.info("generate the export format")
-         # create calculate Expression
+        # create calculate Expression
         self.process_export(start_pages, pages=pages)
-             
+
         logger.info("print the export")
-        self.export(start_pages)
-    
+        self.export(start_pages, conversion_id=conversion_id)
+
     ### walking function
     def process_base(self, start_pages, **kwargs):
         # for each node, check if condition is required issubclass(TriccNodeDisplayModel)
         # process name
-        stashed_node_func(start_pages[self.processes[0]].root, self.generate_base, **{**self.get_kwargs(),**kwargs} )
-        self.do_clean( **{**self.get_kwargs(),**kwargs})
-        
+        stashed_node_func(
+            start_pages[self.processes[0]].root,
+            self.generate_base,
+            **{**self.get_kwargs(), **kwargs}
+        )
+        self.do_clean(**{**self.get_kwargs(), **kwargs})
+
     def process_relevance(self, start_pages, **kwargs):
-        
-        stashed_node_func(start_pages[self.processes[0]].root, self.generate_relevance, **{**self.get_kwargs(),**kwargs} )
-        self.do_clean( **{**self.get_kwargs(),**kwargs})
-        
+        stashed_node_func(
+            start_pages[self.processes[0]].root,
+            self.generate_relevance,
+            **{**self.get_kwargs(), **kwargs}
+        )
+        self.do_clean(**{**self.get_kwargs(), **kwargs})
+
     def process_calculate(self, start_pages, **kwargs):
         # call the strategy specific code
-        stashed_node_func(start_pages[self.processes[0]].root, self.generate_calculate, **{**self.get_kwargs(),**kwargs} )
-        self.do_clean(**{**self.get_kwargs(),**kwargs})
-        
+        stashed_node_func(
+            start_pages[self.processes[0]].root,
+            self.generate_calculate,
+            **{**self.get_kwargs(), **kwargs}
+        )
+        self.do_clean(**{**self.get_kwargs(), **kwargs})
+
     def process_export(self, start_pages, **kwargs):
-        stashed_node_func(start_pages[self.processes[0]].root, self.generate_export, **{**self.get_kwargs(),**kwargs} )
-        self.do_clean(**{**self.get_kwargs(),**kwargs})
-        
- 
+        stashed_node_func(
+            start_pages[self.processes[0]].root,
+            self.generate_export,
+            **{**self.get_kwargs(), **kwargs}
+        )
+        self.do_clean(**{**self.get_kwargs(), **kwargs})
+
     # node function
     @abc.abstractmethod
-    def generate_calculate(self, node, **kwargs ):
-        pass 
+    def generate_calculate(self, node, **kwargs):
+        pass
+
     @abc.abstractmethod
     def generate_base(self, node, **kwargs):
-        pass 
+        pass
 
-    
     @abc.abstractmethod
     def generate_relevance(self, node, **kwargs):
         pass
-    
-        
+
     @abc.abstractmethod
     def generate_export(self, node, **kwargs):
         pass
@@ -85,8 +94,9 @@ class BaseOutPutStrategy:
     def export(self, **kwargs):
         pass
 
-## Utils
+    ## Utils
     def do_clean(self, **kwargs):
         pass
+
     def get_kwargs(self):
-        return {} 
+        return {}
