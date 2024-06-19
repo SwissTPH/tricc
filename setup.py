b7d29c044deaefefeab2a5686b13d5f7bf664d5c
Rukshan <arkruka@gmail.com>
2024-06-19 14:28:16 +0200

    build: set pydantic version

--- setup.py
+++ setup.py
@@ -1,33 +1,33 @@
 from setuptools import setup
 
 setup(
-    name='tricc-oo',
-    version='1.0.2',
-    author='DHU SwissTPH httu.admin@swisstph.ch',
-    description='Python library that converts XLS to Fhir SDC ressource.',
-    long_description='Python library that converts XLS to Fhir SDC ressource.',
-    url='https://github.com/SwissTPH/tricc',
-    keywords='xlsform, drawio, authoring',
-    python_requires='>=3.8, <4',
+    name="tricc-oo",
+    version="1.0.2",
+    author="DHU SwissTPH httu.admin@swisstph.ch",
+    description="Python library that converts XLS to Fhir SDC ressource.",
+    long_description="Python library that converts XLS to Fhir SDC ressource.",
+    url="https://github.com/SwissTPH/tricc",
+    keywords="xlsform, drawio, authoring",
+    python_requires=">=3.8, <4",
     install_requires=[
         "lxml",
         "html2text",
-        "pydantic<2",
+        "pydantic==1.10",
         "babel",
         "xlsxwriter",
         "pandas",
         "polib",
-        "StrEnum"
-        ],
-    #extras_require={
+        "StrEnum",
+    ],
+    # extras_require={
     #    'test': ['pytest', 'coverage'],
-    #},
-    #package_data={
+    # },
+    # package_data={
     #    'sample': ['example_data.csv'],
-    #},
-    #entry_points={
+    # },
+    # entry_points={
     #    'runners': [
     #        'sample=sample:main',
     #    ]
-    #}
+    # }
 )
