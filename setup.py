6ffdfd5cde76cc4a1534eecb3848ddd84de8535e
delcroip <patrick.delcroix@swisstph.ch>
2025-01-13 14:01:48 +0100

    chore: :bookmark: 1.1.15

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="tricc-oo",
-    version="1.1.14",
+    version="1.1.15",
     author="DHU SwissTPH httu.admin@swisstph.ch",
     description="Python library that converts XLS to Fhir SDC ressource.",
     long_description="Python library that converts XLS to Fhir SDC ressource.",
