715e363133aef447a0651f96f833afac0970123c
delcroip <patrick.delcroix@swisstph.ch>
2025-03-19 12:39:26 +0100

    fix: :art: avoid new lines when reading text

--- tricc_oo/converters/utils.py
+++ tricc_oo/converters/utils.py
@@ -39,7 +39,12 @@ def get_rand_name(name=None, length=8):
 
 # the soup.text strips off the html formatting also
 def remove_html(string):
-    text = html2text.html2text(string) # retrive pure text from html
+    h = html2text.HTML2Text()
+    h.body_width = 0  # Prevents line wrapping
+    h.ignore_links = True  # Ignores any link processing
+    h.ignore_images = True  # Ignores image processing
+    h.ignore_tables = True  # Ignores table formatting
+    text = h.handle(string).rstrip()  # rstrip()
     text = text.strip('\n') # get rid of empty lines at the end (and beginning)
     text = text.split('\n') # split string into a list at new lines
     text = '\n'.join([i.strip(' ') for i in text if i]) # in each element in that list strip empty space (at the end of line) 
